b00ba662a8210ee5b24977ac66b52ac93a447326
HomeroMortola <homemortola@gmail.com>
2025-04-29 17:28:55 -0300

    punto 2 prototipo 1

--- 2_distancia_entre_dos_puntos.py
+++ 2_distancia_entre_dos_puntos.py
@@ -1,6 +1,6 @@
 
 while True:
-    unidadez_utilisar = input("ingrese las unidades a utilisar(k=kilometro/a=milla/m=metro)").lower
+    unidadez_utilisar = input("Ingrese las unidades a utilisar(k=kilometro/a=milla/m=metro) :").lower
 
     #comprobamos si la estrada es valida y asignamos la unidad a utilisar
     match unidadez_utilisar:
@@ -17,16 +17,23 @@ while True:
             print("Entrada no valida")
     
 while True:
-    distancia_calcular = input("ingrese la distancia a calcular").lower
+    distancia_calcular = input("Ingrese la distancia a calcular () :").lower
+    #comprobamos si la estrada es valida y asignamos la unidad a utilisar
+    if distancia_calcular == 't' or distancia_calcular == 'e' or distancia_calcular == 'm':
+        break
+    else:
+        print("Entrada no valida")
+
+try:
+    x1 = input("Ingrese la cordenada x del primer punto :")
+    y1 = input("Ingrese la cordenada y del primer punto :")
+    x2 = input("Ingrese la cordenada x del segundo punto :")
+    y2 = input("Ingrese la cordenada y del segundo punto :")
+
     match distancia_calcular:
-        case 't':
-            resultado = 
-        case 
-            distancia_calcular = 'e'
-        case 
-            distancia_calcular = 
-        case 
-            
-        
-    
+        case 'k'
+
+
+except:
+    print("Valore no validos")
 
