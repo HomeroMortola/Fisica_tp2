8ec5391ac29f5a62454886b9346c9b71a17e9036
HomeroMortola <homemortola@gmail.com>
2025-04-29 18:11:51 -0300

    prototipo 2

--- 2_distancia_entre_dos_puntos.py
+++ 2_distancia_entre_dos_puntos.py
@@ -1,4 +1,4 @@
-
+import math
 while True:
     unidadez_utilisar = input("Ingrese las unidades a utilisar(k=kilometro/a=milla/m=metro) :").lower
 
@@ -25,13 +25,19 @@ while True:
         print("Entrada no valida")
 
 try:
-    x1 = input("Ingrese la cordenada x del primer punto :")
-    y1 = input("Ingrese la cordenada y del primer punto :")
-    x2 = input("Ingrese la cordenada x del segundo punto :")
-    y2 = input("Ingrese la cordenada y del segundo punto :")
+    x1 = int(input("Ingrese la cordenada x del primer punto :"))
+    y1 = int(input("Ingrese la cordenada y del primer punto :"))
+    x2 = int(input("Ingrese la cordenada x del segundo punto :"))
+    y2 = int(input("Ingrese la cordenada y del segundo punto :"))
 
     match distancia_calcular:
-        case 'k'
+        case 't':
+            respuesta = abs((x1-x2))+abs(y1-y2)
+        case 'e':
+            respuesta = math.sqrt((x1-x2)**2+(y1-y2)**2)
+        case 'm':
+            respuesta = max((x1-x2),(y1-y2))
+
 
 
 except:
