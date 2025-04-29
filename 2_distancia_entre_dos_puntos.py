3a4afea18af54d2b75557cb1a62ac68f334d7a41
HomeroMortola <124947473+HomeroMortola@users.noreply.github.com>
2025-04-29 17:03:36 -0300

    Add files via upload

---
+++ 2_distancia_entre_dos_puntos.py
@@ -0,0 +1,32 @@
+
+while True:
+    unidadez_utilisar = input("ingrese las unidades a utilisar(k=kilometro/a=milla/m=metro)").lower
+
+    #comprobamos si la estrada es valida y asignamos la unidad a utilisar
+    match unidadez_utilisar:
+        case 'k':
+            unidadez_utilisar = "kilometros"
+            break
+        case 'a':
+            unidadez_utilisar = "milla"
+            break
+        case 'm':
+            unidadez_utilisar = "metros"
+            break
+        case _:
+            print("Entrada no valida")
+    
+while True:
+    distancia_calcular = input("ingrese la distancia a calcular").lower
+    match distancia_calcular:
+        case 't':
+            resultado = 
+        case 
+            distancia_calcular = 'e'
+        case 
+            distancia_calcular = 
+        case 
+            
+        
+    
+
